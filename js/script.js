22ade26a9c916df20ba11579d44873c52f047c6f
Antonio P. <letual@outlook.es>
2024-05-21 13:22:30 +0200

    act2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-// Botón desplazamiento
-(function() {
-    const ishow = 1700
-    const $divtop = document.getElementById("div-totop")
-    window.addEventListener("scroll", function() {
-        if(document.documentElement.scrollTop > ishow){
-            $divtop.style.display = "inherit"
-        }
-        else {
-            $divtop.style.display = "none"
-        }
-    })
-})()
+// JavaScript
+const likeButton = document.getElementById('like-button');
+const likeCountElement = document.getElementById('like-count');
+
+// Check if like count data exists in localStorage and set the count accordingly
+let likeCount = JSON.parse(localStorage.getItem('likeCount')) || 0;
+likeCountElement.innerText = likeCount;
+
+// Listen for clicks on the like button and increment the like count
+likeButton.addEventListener('click', () => {
+    likeCount++;
+    likeCountElement.innerText = likeCount;
+
+    // Store the updated like count in localStorage
+    localStorage.setItem('likeCount', JSON.stringify(likeCount));
+});
